340074dd90a507c2505c9692dba5f0e5b47df68e
月之舞 <1963196695@qq.com>
2020-12-07 22:48:34 +0800

    手写实现apply

---
+++ src/apply.js
@@ -0,0 +1,23 @@
+function myApply(context, ...args) {
+  context = context || window;
+  context.fn = this;
+  let res = context.fn([...args]);
+  delete fn;
+  return res;
+}
+
+Function.prototype.myCall = myCall;
+
+let obj = {
+  name: "tom",
+  getName() {
+    console.log(this.name);
+  },
+};
+var name = "jack";
+
+function getName() {
+  console.log(this.name);
+}
+
+obj.getName.myApply();
